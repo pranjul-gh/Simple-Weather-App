3d19362d6e163d0061a954f94114d9d8a6377901
Pranjul <pranjulmaurya.23@gmail.com>
2023-04-16 00:56:40 +0530

    preview

--- script.js
+++ script.js
@@ -9,23 +9,24 @@ const weather = document.querySelector("#weather")
 // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
 
 const getWeather = async (city)=>{
+    weather.innerHTML = `<h4>fetching..</h4>`
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url)
-    console.log(response)
+   // console.log(response)
     const data = await response.json()
     // console.log(data)
     return showWeather(data)
 }
 
 const showWeather = (data)=>{
-    console.log(data)
+    
     if(data.cod==="404"){
         weather.innerHTML = `<h3>${data.message}</h3>`
         return
     }
     weather.innerHTML = `
     <div>
-        <img src="src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"" alt="">
+        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="clouds.png">
     </div>
     <div>
         <h2> ${data.main.temp} ℃</h2>
